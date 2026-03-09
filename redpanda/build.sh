a1b9859a5c13d5dadb6b2a5722ba3fb2476d2c1a
flobbe9 <florin735@live.com>
2026-03-09 12:00:56 +0100

    fix: minor esm library conversions

--- redpanda/build.sh
+++ redpanda/build.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
+
 # terminate on error
 set -e
 
 OMIT_TESTS="false";
+CLEAN_BUILD="false";
 
 # evaluate flags
 while [ "$#" -gt 0 ]; do
-  case "$1" in
-    --omitTests)
-      OMIT_TESTS="true"
-      shift
-      ;;
-    -*) # unhandled flag
-      echo "Unknown option: $1" >&2
-      exit 1
-      ;;
-  esac
+    case "$1" in
+        --omitTests) # skip jest tests
+        OMIT_TESTS="true"
+        shift
+        ;;
+        --clean) # delete /dist dir before building
+        CLEAN_BUILD="true"
+        shift
+        ;;
+        -*) # unhandled flag
+        echo "Unknown option: $1" >&2
+        exit 1
+        ;;
+    esac
 done
 
-# combile and build
+# ensure a clean /dist dir
+if [ $CLEAN_BUILD = "true" ]; then 
+    rm -rf ./dist;
+fi
+
+# compile and build
 tsc -b;
 
 # test
@@ -26,13 +37,15 @@ if [ $OMIT_TESTS = "false" ]; then
     jest -- ./src;
 else
     echo "Omitting tests";
-fi;
-
-# resolve aliases in .d. files
-tsc-alias &&
+fi
 
 # modify /dist content
-shx cp ./package.json ./dist &&
-shx rm -rf ./dist/src/__tests__ &&
-shx cp ./README.md ./dist &&
-shx cp ./LICENSE ./dist;
+shx rm -rf dist/src/__tests__;
+shx cp package.json ./dist;
+shx cp README.md ./dist;
+shx cp LICENSE ./dist;
+
+# convert '@' aliase imports to relative paths
+tsc-alias;
+# append '.js' to import statements
+tsc-esm-fix;
