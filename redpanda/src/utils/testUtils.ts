3371a219c133b732a69d50f663e1b27e4f874967
flobbe9 <florin735@live.com>
2026-04-02 13:26:26 +0200

    test: stop keepalive on error

--- redpanda/src/utils/testUtils.ts
+++ redpanda/src/utils/testUtils.ts
@@ -87,13 +87,17 @@ export async function expectAsyncNotToThrow(asyncCallback: () => Promise<any>): 
  * Override `setInterval` with a mock function which simply logs and always returns 1.
  * 
  * @param callback execute in mock function
+ * @return 1, the interval id of the mock interval
  */
-export function mockSetInterval(callback?: (_handler: TimerHandler, timeout?: number, ..._arguments: any[]) => void): void {
-    global.setInterval = jest.fn((_handler: TimerHandler, timeout?: number, ..._arguments: any[]) => {
+export function mockSetInterval(callback?: (_handler: TimerHandler, timeout?: number, ..._arguments: any[]) => void): number {
+    const intervalId = 1;
+    global.setInterval = jest.fn((_handler: TimerHandler, timeout?: number, ..._arguments: any[]): any => {
         if (callback)
             callback(_handler, timeout, ..._arguments);
 
         console.log("set mock interval", timeout);
-        return 1 as any; // should be NodeJS.Timeout
+        return intervalId;
     });
+
+    return intervalId;
 }
